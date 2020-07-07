5820f7bbad37cbdf18c8487fe68f71b649d3600a
Artur Brito <artur.brito95@gmail.com>
2020-07-07 22:36:05 +0100

    Commit to test rpm on server

--- routes/facebook.js
+++ routes/facebook.js
@@ -54,8 +54,6 @@ router.get('/logout', function (req, res) {
 
 // Middleware verification
 function ensureAuthenticated(req, res, next) {
-    console.log(req.headers);
-    console.log(req.isAuthenticated());
     if (req.isAuthenticated()) { return next(); }
     //res.redirect('/api/facebook/login')
     res.status(503).send('Not logged in');
